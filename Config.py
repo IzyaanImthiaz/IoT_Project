fcf9b5333f01d78b442d32e0bd8bae6b3815bbb5
Puranjanaaa <puranja@gmail.com>
2023-05-07 14:11:00 +0530

    Fine Tune

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@ buzzer=23
 servo=24
 sensor=25
 
-hi_lowlimit =60
+hi_lowlimit =70
 hi_highlimit = 140
 
 servo_minangle = 0
@@ -12,3 +12,5 @@ mqtt_broker = "broker.hivemq.com"
 mqtt_port = 1883
 
 prediction_year_range = 1
+
+model_loc = "Desktop/IOT/iotbda-assignment-2023-2023_030_team/Model/"
--- Hardware/Predict.py
+++ Hardware/Predict.py
@@ -4,14 +4,18 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
 import pickle
+import numpy as np
 
-def forecst(element_name, current_rh, start_date, end_date):
+def forecst(loc, element_name, current_rh, start_date, end_date):
 
     print("Predicting for {}...".format(element_name))
-    with open(str(element_name)+'.pkl', 'rb') as f:
+    with open(loc + str(element_name)+'.pkl', 'rb') as f:
         model_fit = pickle.load(f)
-
-    forecast_rh = model_fit.predict(start=start_date, end=end_date, dynamic=True, exog=current_rh)
+   
+    forecast_rh = model_fit.predict(start=start_date, end=end_date)
+    # forecast_rh = model_fit.predict(start=start_date, end=end_date, dynamic=True, exog=current_rh)
+    # modified_values = np.where(current_rh > 0.5, forecast_rh*1.2, forecast_rh)
+    
     print(f'The 95% confidence interval for the forecasted {element_name} values is:')
     print(forecast_rh)
     
